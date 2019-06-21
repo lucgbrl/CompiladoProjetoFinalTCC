b8f6051b63fba8d757d09ec1a5b78a158347260a
lucgbrl <lucassantos@alu.ufc.br>
2019-06-20 23:39:02 -0300

    Corrigindo Navbar
    Navbar atualizada

--- nav.php
+++ nav.php
@@ -19,7 +19,7 @@
 				</li-->
 			</ul>
 			<form class="form-inline my-2 my-lg-0">
-				<button class="btn btn-success my-2 my-sm-0" type="submit"><i class = "fa fa-user-circle"></i> Github</button>
+				<a target = "blank" href = "https://github.com/lucgbrl/CompiladoProjetoFinalTCC" class="btn btn-outline-light my-2 my-sm-0"><i class = "fa fa-user-circle"></i> Github</a>
 			</form>
 		</div>
 	</div>
