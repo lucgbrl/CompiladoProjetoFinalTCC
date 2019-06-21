5aea90b75c4b51d3a5c4e84292f235ce72d5a409
lucgbrl <lucassantos@alu.ufc.br>
2019-06-20 23:36:53 -0300

    criando links
    adicionando links na barra superior

--- pages/nav.php
+++ pages/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg" id = "nav">
   <div class = "container">
-  <a class="navbar-brand" href="#">Lucas Gabriel</a>
+  <a class="navbar-brand" href="./index.php">Lucas Gabriel</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -8,13 +8,13 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto text-primary">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Início <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="./index.php">Início <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Galeria 01</a>
+        <a class="nav-link" href="./feed.php">Galeria 01</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Galeria 02</a>
+        <a class="nav-link" href="./feed_02.php">Galeria 02</a>
       </li>
       <!--li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -33,7 +33,7 @@
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <!--input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"-->
-      <button class="btn btn-outline-primary my-2 my-sm-0" type="submit"><i class = "fab fa-github"></i> GitHub</button>
+      <a href = "https://github.com/lucgbrl/CompiladoProjetoFinalTCC"><button class="btn btn-outline-primary my-2 my-sm-0" type="submit"><i class = "fab fa-github"></i> GitHub</button></a>
     </form>
   </div>
 </div>
